b89ee0eacc261baf5423c4a9154b8891ccf046b0
Rajkov9 <rajkov.ivan24@gmail.com>
2025-05-05 14:56:15 +0200

    footer design completed and changed the photo from a png to a svg

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,6 +1,19 @@
 const Footer = () => {
     return `
-        <p>This is the footer.</p>
+    <div class="socialIcons">
+            <a href="https://www.facebook.com/people/When-in-Trouble/100080249001608/?mibextid=wwXIfr&rdid=Ul0d1ZYfnMrRCgZJ&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1AQtQoFg3s%2F%3Fmibextid%3DwwXIfr"
+                target="blank"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/troublegostivar2022/?igsh=N3c0d3E3emtlODI%3D#" target="blank"><i
+                    class="bi bi-instagram"></i></a>
+        </div>
+        <div>
+            <p>Sretko Krsteski 1232 - Gostivar, Republic of North Macedonia</p>
+            <div class="mailSubSection">
+                <i class="bi bi-envelope-fill"></i>
+                <p>troublegostivar@gmail.com</p>
+            </div>
+            <img src="./src/assets/when-in-trouble-logo-cmyk.svg">
+        </div>
     `;
 };
 
